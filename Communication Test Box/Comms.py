1cc946a6914be55a1ed908da3640b6a6f3f225a2
Cordeiro <joao.cordeiro@ua.pt>
2022-12-08 11:46:19 +0000

    Test box Description added

--- Communication Test Box/Comms.py
+++ Communication Test Box/Comms.py
@@ -1,1 +1,13 @@
+# Commands to Communicate:
+    # '1 A' - Connect System
+    # '2 A' - Read Modules
+    # '3 A' - Start Test
+    # '4 A' - Tracking Results
+
+# criar comunicação TCP com a caixa (default)
+# • IP address: 127.0.0.1 
+# • Port number: 13000 
+# • Transfer data buffer(size): 8192 bytes 
+
+
 print ('teste git')
