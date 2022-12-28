c6fd9f7d11735516da85b432164e713a688d9f00
leonardo-ra <94324481+leonardo-ra@users.noreply.github.com>
2022-12-28 18:24:44 +0000

    Main_struct_2.0
    O que tem checkmarks é o que ainda não foi implementado no polyscope.

--- Control/Main_struct.py
+++ Control/Main_struct.py
@@ -1,16 +1,19 @@
-
-
 while ROBOT Program:
    
-    resets variables
+   ✓ #resets variables
+   ✓ Loop(1) 
+   ✓ #conn=false, xfp, etc... ✓
     
-    while connection_with_computer == False:
-        try_to_connect
-       
-    sends trigger
-    test = communicate_with_test_box()
+   ✓ #Confirms communication with computer
+   ✓ while connection_with_computer == False:
+   ✓     try_to_connect 
+   ✓ sends trigger #"test done?"
     
-    if any test ended:
+   ✓ loop test_done[0] = 0
+   ✓  waits for reply from trigger
+    
+   ✓ #"In case a test has finished" 
+   ✓ if test_done = 1:
 
         go to test box fixed position 
 
@@ -33,41 +36,55 @@ while ROBOT Program:
             Drop_in_fixed point
 
 
-    if no test ended:
-
-        go to XFP board fixed position 
-
-        sends trigger
-        Position_of_XFP, Side_of_XFP = input_from_computer(find_xfp)
-
-        get close to Position_of_XFP
+    ✓ if test_done = 0:
+     subprogram(go_to_xfp_position)
+     {
+     ✓ go to default initial position to read XFP #WapointX (substituir com o número do waypoint final)
+     ✓ sends trigger "take xfp photo"
+     ✓ xfp_data = data_from_foto_taken # PolyscopeVar, X, Y, Alpha, Side, !!!!!!!Push!!!!!!!
+     ✓                                 # xfp_data[0], xfp_data[1], xfp_data[2], xfp_data[3], xfp_data[4], xfp_data[5] - order of variables received
+     ✓ 
+     ✓ xfp_position = poseTrans(plane_camera, p[xfp_data[1]/1000, xfp_data[2]/1000, -0.01, 0, 0, xfp_data[3]]) #0,0 represent the Rx, Ry. The -0.01 lets the arm hover above the XFP.
+     ✓   moveJ
+     ✓    xfp_position #moves the arm to the translated pose
+     ✓    
+     ✓ xfp_side = xfp_data[4]
+     } 
+      
+     ✓ if xfp_side == 3: # if Side = 3, meaning the XFP is on its side
+        ✓ if xfp_data[5]==1 #must adjust in X axis
+          ✓  xfp_position = poseTrans(plane_camera, p[(xfp_data[1]/1000 + 0.05), xfp_data[2]/1000, -0.01, 0, 0, xfp_data[3]]) #0,0 represent the Rx, Ry. The -0.01 lets the arm hover above the XFP.
+         
+        ✓ elif xfp_data[5]==2 #must adjust in Y axis
+        ✓    xfp_position = poseTrans(plane_camera, p[(xfp_data[1]/1000), (xfp_data[2]/1000 + 0.05), -0.01, 0, 0, xfp_data[3]]) #0,0 represent the Rx, Ry. The -0.01 lets the arm hover above the XFP.
+        ✓ moveJ
+         ✓   xfp_position
+     ✓ else
+       ✓ xfp_position = poseTrans(plane_camera, p[xfp_data[1]/1000, xfp_data[2]/1000, 0, 0, 0, xfp_data[3]]) #lowers the arm to be within reach of the XFP
+        ✓    moveJ
+         ✓     xfp_position  #moves the arm to the translated pose
+       ✓ Grip(0)
+         moveJ
+            #Waypoint_suporte para pousar o xfp
+         Release(37mm)
+         if(xfp_side == 1) # Meaning XFP is 'front'
+            moveJ
+               #Waypoint_toGrabFromEnd
+            Grip(0)
+            moveJ
+               #Waypoint_TestBoxPOV
+        elif(xfp_side == 2) # Meaning XFP is 'back'
+            moveJ
+               #Waypoint_toGrabFromEnd
+            subprogram(TurnXFP) #Subprogram has Grip(0), and Waypoint_TestBoxPOV inside
         
-        confirm position??
+         send_trigger("Find_Free_Port") #Takes photo of the box and calulates free port
+         Location_of_port = input_from_computer(find_free_port)
 
-        if Side_of_XFP == front:
-            grab XFP
+         go to Location_of_port
 
-        if Side_of_XFP == back:
-            adjust XFP 
-            grab XFP
-
-        if Side_of_XFP == side:
-            adjust XFP 
-            grab XFP
-
-        go to test box fixed position 
-
-        sends trigger
-        Location_of_port = input_from_computer(find_free_port)
-
-        go to Location_of_port
-
-        insert XFP
-        lock removal mechanism
-        adds optic fiber
-
-    reset_conection_with_coputer
-
-
-
-
+         insert XFP
+         lock removal mechanism
+         adds optic fiber       
+ }
+   ✓ reset_conection_with_coputer
