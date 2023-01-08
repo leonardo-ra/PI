173255cebd9ade02a43af5a014bd6df6b0e0227e
Cordeiro <joao.cordeiro@ua.pt>
2023-01-08 20:09:41 +0000

    test_done_test.py with test box results

--- Control/test_done_test.py
+++ Control/test_done_test.py
@@ -1,6 +1,12 @@
 import socket
 import time
+import os
+import sys
 
+script_dir = os.path.dirname( __file__ )
+mymodule_dir = os.path.join( script_dir, '..', 'Comms_Test_Box')
+sys.path.append( mymodule_dir )
+import TestBoxComms as Tbox
 HOST = "192.1.1.2"   # The remote host
 PORT = 6000     # The same port as used by the server
 
@@ -12,8 +18,24 @@ s.bind((HOST, PORT))      # Bind to the port
 s.listen()              # Wait for client connection
 c, addr = s.accept()    # Establish connection with client
 
-while True:
-    
+
+def getResults():
+    slotRes, moreInfo = Tbox.run()  # ao chamar isto, o pc passa a comunicar com a caixa de testes, e só sai daqui quando os testes terminam todos.
+    # portanto, se isto termina é porque test done. entao podemos enviar diretamente o slot e o resultado
+
+    # transforma o resultado no formato 1 ou 0
+    for i in range(4):
+        if slotRes['Slot '+str(i+1)] == 'PASS':
+            slotRes['Slot '+str(i+1)] = 1
+        elif slotRes['Slot '+str(i+1)] == 'FAIL':
+            slotRes['Slot '+str(i+1)]  = 0
+    return slotRes
+
+result = getResults()
+i = 1
+while i <= 4:
+    # Send slot and result to the arm
+    tdone = 1   # test is done
     try:
         msg = c.recv(1024)
         msg = msg.decode()
@@ -21,7 +43,7 @@ while True:
 
         if msg == "test done?":
             # envia um para indicar que um teste acabou
-            tuplo = (1,1)   # o 1 extra e porque o polyscope precisa ou nao funciona
+            tuplo = (tdone,1)   # o 1 extra e porque o polyscope precisa ou nao funciona
             string = str(tuplo)
             print(string)
             string2 = string.encode()
@@ -29,7 +51,9 @@ while True:
             print("Sent "+string)
 
         elif msg == "test position":
-            tuplo = (86,12.56,1)
+
+            tuplo = (86,12.56,result['Slot '+str(i)])
+            # deve calcular aqui a posição do slot?, ou no polyscope, já sabe qual a pos de cada slot, só temos de enviar o numero do slot?
             string = str(tuplo)
             print(string)
             string2 = string.encode()
@@ -39,6 +63,8 @@ while True:
         time.sleep(1)
     except socket.error as socketerror:
         break
+
+    i += 1
 
 s.close()
 c.close()
