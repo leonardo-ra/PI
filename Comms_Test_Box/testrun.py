a609a8a4b150960ce9be5f6e27212a2835126b3f
Cordeiro <joao.cordeiro@ua.pt>
2023-01-09 18:59:06 +0000

    test done test

--- Comms_Test_Box/testrun.py
+++ Comms_Test_Box/testrun.py
@@ -2,8 +2,8 @@
 # supported languages: Portuguese and English
 import TestBoxComms as TBox
 
-slotRes, moreInfo = TBox.run('Portuguese')
-print(slotRes)
-print(slotRes['Slot 1'] == 'PASS')
+slotRes, Info = TBox.run('Portuguese')
+print(slotRes,'\n',Info)
+#print(slotRes['Slot 1'] == 'PASS')
 
 #print(TextBoxComms.string_example)
--- Control/test_done_test.py
+++ Control/test_done_test.py
@@ -12,59 +12,67 @@ PORT = 6000     # The same port as used by the server
 
 print("Starting program")
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+'''s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 s.bind((HOST, PORT))      # Bind to the port  
 s.listen()              # Wait for client connection
 c, addr = s.accept()    # Establish connection with client
+'''
 
-
-def getResults():
-    slotRes, moreInfo = Tbox.run()  # ao chamar isto, o pc passa a comunicar com a caixa de testes, e só sai daqui quando os testes terminam todos.
+def getResults(Check = 0):
+    slotRes, info = Tbox.run(resultCheck = Check)  # ao chamar isto, o pc passa a comunicar com a caixa de testes, e só sai daqui quando os testes terminam todos.
     # portanto, se isto termina é porque test done. entao podemos enviar diretamente o slot e o resultado
-
+    if info == ('TESTS RUNNING' or 'TRANSCEIVERS MISSING'):
+        info = 0
+        return slotRes,info
+    else:
+        info = 1
     # transforma o resultado no formato 1 ou 0
-    for i in range(4):
-        if slotRes['Slot '+str(i+1)] == 'PASS':
-            slotRes['Slot '+str(i+1)] = 1
-        elif slotRes['Slot '+str(i+1)] == 'FAIL':
-            slotRes['Slot '+str(i+1)]  = 0
-    return slotRes
+        for i in range(2):
+            if slotRes['Slot '+str(i+1)] == 'PASS':
+                slotRes['Slot '+str(i+1)] = 1
+            elif slotRes['Slot '+str(i+1)] == 'FAIL':
+                slotRes['Slot '+str(i+1)]  = 0
+        return slotRes, info
 
-result = getResults()
+#result = getResults()
 i = 1
-while i <= 4:
+while True:
     # Send slot and result to the arm
-    tdone = 1   # test is done
     try:
-        msg = c.recv(1024)
-        msg = msg.decode()
-        print(f"Received: {msg}")
-
+        #msg = c.recv(1024)
+        #msg = msg.decode()
+        #print(f"Received: {msg}")
+        #msg = "test done?"
+        msg = "test position"
+        getResults()
         if msg == "test done?":
+            result, tdone = getResults(Check = 1)
             # envia um para indicar que um teste acabou
             tuplo = (tdone,1)   # o 1 extra e porque o polyscope precisa ou nao funciona
             string = str(tuplo)
             print(string)
-            string2 = string.encode()
-            c.send(string2)
+            #string2 = string.encode()
+            #c.send(string2)
             print("Sent "+string)
 
         elif msg == "test position":
-
-            tuplo = (86,12.56,result['Slot '+str(i)])
+            if i == 1:  # se for a primeira vez que pede a posição
+                result, tdone = getResults(Check = 1)
+            slotn = 'Slot '+str(i)
+            print(slotn)
+            tuplo = (86,12.56,result[slotn])
             # deve calcular aqui a posição do slot?, ou no polyscope, já sabe qual a pos de cada slot, só temos de enviar o numero do slot?
             string = str(tuplo)
             print(string)
-            string2 = string.encode()
-            c.send(string2)  #mandar o X, Y e o resultado do teste 1 para OK e 2 para NOK
+            #string2 = string.encode()
+            #c.send(string2)  #mandar o X, Y e o resultado do teste 1 para OK e 2 para NOK
             print("Sent "+string)
+            i += 1
 
         time.sleep(1)
     except socket.error as socketerror:
         break
 
-    i += 1
-
-s.close()
-c.close()
+#s.close()
+#c.close()
