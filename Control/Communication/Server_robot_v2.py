8cb86d697f9497226b0180849f60779194c56039
Leo R <leonardo.r@ua.pt>
2023-01-18 20:24:17 +0000

    alteracoes

--- Control/Communication/Server_robot_v2.py
+++ Control/Communication/Server_robot_v2.py
@@ -17,24 +17,24 @@ while True:
         msg = c.recv(1024)
         msg = msg.decode()
         print(f"Received: {msg}")
-        if msg == "test done?":
+        if msg == "ready":
             #o número enviado deste lado (do python) tem de ter também o número de variáveis a enviar, neste caso 1
-            test_yes=(1,1) #trigger para avançar do "test done?"
-            test_yes = str(test_yes) #transforma em string
-            c.send(test_yes.encode()) #mandar variaveis. Ainda não percebi muito bem porque é que ele n as lê do lado do Polyscope
-            msg = c.recv(1024)
-            msg = msg.decode()
-            print(f"Received: {msg}")
+            #test_yes=(1,1) #trigger para avançar do "test done?"
+            #test_yes = str(test_yes) #transforma em string
+            #c.send(test_yes.encode()) #mandar variaveis. Ainda não percebi muito bem porque é que ele n as lê do lado do Polyscope
+            #msg = c.recv(1024)
+            #msg = msg.decode()
+            #print(f"Received: {msg}")
             #Posição adquirida pela câmara e calculada pela câmara, a nossa "test position"
             #exemplo:
             #numero de variaveis enviadas: 6... Mas segundo o Pedro se calhar deveriam ser 3 (X,Y,Alpha)
-            test_pos=(3.45, 4.81, -6.22, 0.00, 0.00, -0.035) 
+            test_pos=(3.45, 4.81,-0.035) 
             test_pos=str(test_pos)
             c.send(test_pos.encode())
             #
-            test_res=(1,1)
-            test_res=str(test_res)
-            c.send(test_res.encode())
+            #test_res=(1,1)
+            #test_res=str(test_res)
+            #c.send(test_res.encode())
             
             
     except socket.error as socketerror:
